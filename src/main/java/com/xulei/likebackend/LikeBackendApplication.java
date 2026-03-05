608964c23ce29462b61c63eb1de1850bf7288789
许磊 <1677702301@qq.com>
2026-03-05 22:23:43 +0800

    点赞项目项目初始化

---
+++ src/main/java/com/xulei/likebackend/LikeBackendApplication.java
@@ -0,0 +1,20 @@
+package com.xulei.likebackend;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class LikeBackendApplication {
+
+    public static void main(String[] args) {
+        run(args);
+    }
+
+
+    static void run(String[] args){
+        SpringApplication.run(LikeBackendApplication.class,args);
+        log.info("app start...");
+    }
+}
